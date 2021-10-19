1a97b8db199a95f5b4c2d1c6dad3a1eb2321ed3a
ly132806 <ly132806@163.com>
2021-10-20 00:16:38 +0800

    Add files via upload

---
+++ 每次调用函数加一.c
@@ -0,0 +1,17 @@
+﻿#include <stdio.h>
+#include <math.h>
+void add(*p)
+{
+	(*p)++;
+}
+int main()
+{
+	int num =0;
+	add(&num);
+	printf("%d\n",num);
+	add(&num);
+	printf("%d\n",num);
+	add(&num);
+	printf("%d\n",num);
+	return 0;
+}
---
+++ 递归斐波那契数.c
@@ -0,0 +1,22 @@
+﻿#include <stdio.h>
+//#include <math.h>
+fib(int a)
+{
+	//int b = 0;
+	if(a <= 2)
+	{
+		return 1;
+	}
+	else
+	{
+		return fib(a-1)+fib(a-2);
+	}
+}
+int main()
+{
+	int n = 0;
+	scanf("%d",&n);
+	int art = fib(n);
+	printf("%d",art);
+	return 0;
+}
