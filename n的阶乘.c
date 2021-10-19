cee0825cb3e9cc9617da56126604495f3d8b7bf4
ly132806 <ly132806@163.com>
2021-10-19 16:17:15 +0800

    Add files via upload

---
+++ n的阶乘.c
@@ -0,0 +1,20 @@
+﻿#include <stdio.h>
+#include <math.h>
+int jie(int a)
+{
+	if(a <= 1)
+	{
+		return 1;
+	}
+	else
+	{
+		return jie(a-1)*jie(a-2);
+	}
+}
+int main()
+{
+	int n = 0;
+	scanf("%d",&n);
+	printf("%d",jie(n));
+	return 0;
+}
---
+++ 阶乘和.cpp
@@ -0,0 +1,14 @@
+﻿#include<stdio.h>
+int main()
+{
+    int n = 1;
+    int j = 1;
+    int num = 0;
+    for(n=1;n<10;n++)
+    {
+        j*=n;
+        num+=j;
+    }
+    printf("%d",num);
+	return 0;
+}
