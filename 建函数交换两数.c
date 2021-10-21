0d4d493131d17812d6d6324c9c7476cae0eefbbc
ly132806 <ly132806@163.com>
2021-10-21 20:54:01 +0800

    Add files via upload

---
+++ 建函数交换两数.c
@@ -0,0 +1,17 @@
+﻿#include <stdio.h>
+#include <math.h>
+void jiaohuan(*pa,*pb)
+{
+	int z = 0;
+	z =*pa;
+	*pa = *pb;
+	*pb = z;
+}
+int main()
+{
+	int a = 10;
+	int b = 20;
+	jiaohuan(&a,&b);
+	printf("%d %d",a,b);
+	return 0;
+}
