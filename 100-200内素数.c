8eda939a3d41a246265800045bdc723b9b21fed9
ly132806 <ly132806@163.com>
2021-10-22 22:07:14 +0800

    Add files via upload

---
+++ 100-200内素数.c
@@ -0,0 +1,29 @@
+﻿#include <stdio.h>
+#include <math.h>
+int main()
+{
+	int i = 0;
+	int a = 0;
+	for(i = 101;i < 200;i+=2)
+	{
+		int n = 0;
+		int s = 1;
+		for(n = 2;n <= sqrt(i);n++)
+		{
+			if(i % n == 0)
+			{
+				//printf("shi=%d",i);
+				s = 0;
+				break;
+			}
+			//else printf("%d ",i);
+		}
+		if(s == 1)
+		{
+			printf("%d ",i);
+			a++;
+		}
+	}
+	printf("\n共有素数%d",a);
+	return 0;
+}
---
+++ 学函数.c
@@ -0,0 +1,7 @@
+﻿#include <stdio.h>
+#include <math.h>
+int main()
+{
+	
+	return 0;
+}
