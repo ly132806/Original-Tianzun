fcaab98c615c00b1499b7c412c952582185f82f9
ly132806 <ly132806@163.com>
2021-10-25 22:04:08 +0800

    Add files via upload

---
+++ 打印100内奇数.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    for(n=1; n<101; n++)
+    {
+        if (n % 2) 
+         printf("%d  ",n);
+       // else
+       // printf("not");
+    }
+    return 0;
+    
+}
