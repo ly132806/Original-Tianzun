f6b3caec13263dd6cf275766028c1025e0d8c3d4
ly132806 <ly132806@163.com>
2021-10-28 00:00:01 +0800

    Add files via upload

---
+++ 输出最大数排序.c
@@ -0,0 +1,28 @@
+﻿#include <stdio.h>
+int main()
+{
+	int a = 0;
+	int b = 0;
+	int c = 0;
+	scanf("%d %d %d",&a,&b,&c);
+	if(a<b)
+	{
+		int map = b;
+		b = a;
+		a = map;
+	}
+	if(a<c)
+	{
+		int map = c;
+		c = a;
+		a = map;
+	}
+	if(b<c)
+	{
+		int map = c;
+		c = b;
+		b = map;
+	}
+	printf("%d %d %d",a,b,c);
+	return 0;
+}
