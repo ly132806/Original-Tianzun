293e3b7ba25e59f19ed82209b9ec55e59d4b128c
ly132806 <ly132806@163.com>
2021-10-28 00:11:23 +0800

    Add files via upload

---
+++ 求最大公约数.c
@@ -0,0 +1,25 @@
+﻿#include <stdio.h>
+int main()
+{
+	int n = 0;
+	int m = 0;
+	int max = 0;
+	scanf("%d%d",&n,&m);
+	if(m < n)
+	{
+		max = m;
+	}
+	else max = n;
+	while(1)
+	{
+	   // break;
+		if(((m % max) == 0) && ((n % max) == 0))
+		{
+			break;
+		}
+		max--;
+	//	break;
+	}
+	printf("%d",max);
+	return 0;
+}
