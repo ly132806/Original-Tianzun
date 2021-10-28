8f4c65efb8a0d40af7cfba6fd6bf796e11c9816a
ly132806 <ly132806@163.com>
2021-10-28 23:44:18 +0800

    Add files via upload

---
+++ 建函数分支练习.c
@@ -0,0 +1,19 @@
+﻿#include <stdio.h>
+int func(int a)
+{
+	int b = 0;
+	switch(a)
+	{
+		case 1: b = 30;
+		break;
+		case 2: b = 20;
+		case 3: b = 5;
+		default: b = 0;
+	}
+	return b;
+}
+int main()
+{
+	printf("%d",func(1));
+	return 0;
+}
