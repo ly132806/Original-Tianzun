ce43d71f8440bc9b26dd5a78f657155e68380faf
ly132806 <ly132806@163.com>
2021-11-02 06:49:05 +0800

    Add files via upload

---
+++ 递归nk次方.c
@@ -0,0 +1,27 @@
+﻿#include <stdio.h>
+#include <math.h>
+int mult(int a,int b)
+{
+	if(b == 0)
+	{
+		return 1;
+	}
+	/*else if(b == 1)
+		{
+			return a;
+		}
+	*/
+	else
+		return a * mult(a,b-1);
+	
+}
+int main()
+{
+	int n = 0;
+	int k = 0;
+	int m = 0;
+	scanf("%d%d",&n,&k);
+	m = mult(n,k);
+	printf("%d",m);
+	return 0;
+}
