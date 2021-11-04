4a6d758c376874a7ba37d202d9295a61ac1d4e52
ly132806 <ly132806@163.com>
2021-11-04 20:25:26 +0800

    Add files via upload

---
+++ 递归数字每位和.c
@@ -0,0 +1,19 @@
+﻿#include <stdio.h>
+int Digitsum(int num)
+{
+	int sum = 0;
+	if(num<10)
+	{
+		return sum = sum + num;
+	}
+	else
+return sum = num % 10 + Digitsum(num/10);
+}
+
+int main()
+{
+	int num = 0;
+	scanf("%d",&num);
+	printf("%d",Digitsum(num));
+	return 0;
+}
---
+++ 递归顺序打印数的每位.c
@@ -0,0 +1,16 @@
+﻿#include <stdio.h>
+void bit(unsigned  int n)
+{
+	if(n > 9)
+	{
+		bit(n / 10);
+	}
+	printf("%d ",n % 10);
+}
+int main()
+{
+	unsigned  int a = 0;
+	scanf("%u ",&a);
+	bit(a);
+	return 0;
+}
