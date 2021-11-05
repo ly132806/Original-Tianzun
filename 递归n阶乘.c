9c41fb92b0c213d69734f75c53b3d0bf329ffba3
ly132806 <ly132806@163.com>
2021-11-05 21:07:37 +0800

    Add files via upload

---
+++ 递归n阶乘.c
@@ -0,0 +1,20 @@
+﻿#include <stdio.h>
+int jie(int n)
+{
+	
+	if(n > 1)
+	{
+		n = jie(n-1) * n;
+		//return n;
+		//jie(n-1);
+	}
+	//int m = 1;
+	return n;
+}
+int main()
+{
+	int n = 6;
+	scanf("%d",&n);
+	printf("%d",jie(n));
+	return 0;
+}
