68887a8af9010d913be38801dd38241b0d3c0234
ly132806 <ly132806@163.com>
2021-11-07 00:26:44 +0800

    Add files via upload

---
+++ 递归字符串长度.c
@@ -0,0 +1,16 @@
+﻿#include <stdio.h>
+int my_strlen(char *arr)
+{
+	if(*arr != '\0')
+	{
+		return 1 + my_strlen(arr+1);;
+	}
+	else
+	return 0;
+}
+int main()
+{
+	char arr[] = "asdfg";
+	printf("%d",my_strlen(arr));
+	return 0;
+}
