25e6d9750e1bbfc94f2c2c9335c73ad08bd8df7d
ly132806 <ly132806@163.com>
2021-11-07 21:35:39 +0800

    Add files via upload

---
+++ 打印乘法表.c
@@ -0,0 +1,15 @@
+﻿#include <stdio.h>
+int main()
+{
+	int i = 0;
+	int n = 0;
+	for(i = 1;i < 10;i++)
+	{
+		for(n = 1;n <= i;n++)
+		{
+			printf("%d*%d=%-2d ",i,n,i * n );
+		}
+		printf("\n");
+	}
+	return 0;
+}
