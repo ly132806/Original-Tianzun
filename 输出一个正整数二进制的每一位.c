7a934e31e555290de2b0a0ccb2d45c9f0016f7b9
ly132806 <ly132806@163.com>
2021-11-14 00:33:13 +0800

    Add files via upload

---
+++ 输出一个正整数二进制的每一位.c
@@ -0,0 +1,33 @@
+﻿#include <stdio.h>
+void pri(int n)
+{
+	if(n != 0)
+	{
+		int k =0;
+		k = n & 1;
+		pri(n >> 1);
+		printf("%d ",k);
+		
+	}
+	//
+	
+}
+int main()
+{
+	int n = 1;
+	//int  m =0;
+	scanf("%d",&n);
+	if( n != 0)
+	{
+		pri(n);
+	}
+	else
+	{
+		printf("%d\n",0);
+	}
+	return 0;
+}
+		
+	
+	
+
