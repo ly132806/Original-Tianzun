4c9aaa667daec43aa5e6ba0e2e3c85603813cd23
ly132806 <ly132806@163.com>
2021-11-14 00:34:49 +0800

    Add files via upload

---
+++ 输出一个整数的每一位.c
@@ -0,0 +1,18 @@
+﻿#include <stdio.h>
+int main()
+{
+	int n = 1;
+	//int  m =0;
+	scanf("%d",&n);
+	do
+	{
+		//m = n % 10;
+		printf("%d ",n % 10);
+		n = n / 10;
+	}while(n);
+	return 0;
+}
+		
+	
+	
+
