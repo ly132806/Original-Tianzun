ef726fd566f89b3950640a23449741cf53510800
ly132806 <ly132806@163.com>
2021-11-13 22:31:57 +0800

    Add files via upload

---
+++ 求一个整数二进制中有几个1.c
@@ -0,0 +1,18 @@
+﻿#include <stdio.h>
+int main()
+{
+	int a = 3892;
+	int count = 0;
+	int n = 32;
+	while(n)
+	{
+		if(a & 1 == 1)
+			{
+				count++;
+			}
+		a = a >> 1;
+		n--;
+	}
+	printf("%d\n",count);
+	return 0;
+}
---
+++ 计算两个int数二进制有几位个不同.c
@@ -0,0 +1,27 @@
+﻿#include <stdio.h>
+int count(int n)
+{
+	int coun = 0;
+	int i = 32;
+	while(i)
+	{
+		if(n & 1 == 1)
+		{
+			coun++;
+		}
+		n = n >> 1;
+		i--;
+	}
+	return coun;
+}
+int main()
+{
+	int m = 0;
+	int n = 0;
+	int c = 0;
+	scanf("%d %d",&m,&n);
+	c =m ^ n;
+	//printf("%d\n",c);
+	printf("%d\n",count(c));//打印n中1个数
+	return 0;
+}
