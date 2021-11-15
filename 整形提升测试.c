810f38794149a4bd90c2bcadd9308de3e96444d7
ly132806 <ly132806@163.com>
2021-11-15 21:32:42 +0800

    Add files via upload

---
+++ 整形提升测试.c
@@ -0,0 +1,16 @@
+﻿#include <stdio.h>
+int main()
+{
+	char a = 1;
+	printf("%u\n",sizeof(a));
+	printf("%u\n",sizeof(+a));
+	printf("%u\n",sizeof(-a));
+	printf("%u\n",sizeof(!a));
+	short s = 5;
+	printf("%d\n",sizeof(s=a+3));
+	printf("%u\n",sizeof(+s));
+	s = (++a)+(++a)+(++a);
+	printf("%d\n",a);
+	printf("%d\n",s);
+	return 0;
+}
