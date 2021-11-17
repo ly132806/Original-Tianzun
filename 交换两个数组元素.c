25ab029b72d0d41eae833ca61193ee7b22c41dc0
ly132806 <ly132806@163.com>
2021-11-17 21:44:38 +0800

    Add files via upload

---
+++ 交换两个数组元素.c
@@ -0,0 +1,34 @@
+﻿#include <stdio.h>
+void reve(arr[],brr[],sz)//交换
+{
+	int i = 0;
+	for(i =0;i < sz;i++)
+	{
+		int amp = 0;
+		amp = arr[i];
+		arr[i] = brr[i];
+		brr[i] = amp;
+	}
+}
+void print(int arr[],int sz)//打印
+{
+	int i = 0;
+	for(i=0;i < sz;i++)
+	{
+		printf("%2d ",arr[i]);
+	}
+	printf("\n");
+}
+int main()
+{
+	int arr[] = {1,2,3,4,5,6,7,8,9,10};
+	int brr[] = {10,9,8,7,6,5,4,3,2,1};
+	
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	print(arr,sz);
+	print(brr,sz);
+	reve(arr,brr,sz);
+	print(arr,sz);
+	print(brr,sz);
+	return 0;
+}
