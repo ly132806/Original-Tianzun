606df9f2602b94e4af0ce7bb5181447d4bb52215
ly132806 <ly132806@163.com>
2021-11-18 23:30:53 +0800

    Add files via upload

---
+++ 分别输出正整数的偶奇位.c
@@ -0,0 +1,40 @@
+﻿#include <stdio.h>
+void out(int a)
+{
+	int i = 1;
+	printf("偶");
+	printf("   奇\n");
+	for(i = 1;i <= 32;i++)
+	{
+		int n = a & 1;
+		
+		if(i%2)
+		{
+			printf("%d",n);
+		}
+		else
+		{
+			printf("    %d\n",n);
+		}
+	a = a >> 1;
+	}
+	/* printf("\n");
+	printf("奇数位:");
+	for(i = 1;i <= 32;i++)
+	{
+		int n = a & 1;
+		
+		if(!(i%2))
+		{
+			printf("%d ",n);
+		}
+	a = a >> 1;
+	} */
+}
+int main()
+{
+	int a = 0;
+	scanf("%d",&a);
+	out(a);
+	return 0;
+}
