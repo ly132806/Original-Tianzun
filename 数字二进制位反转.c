3fdb1788fbbb374dcf45f3d19b16287703b4a64a
ly132806 <ly132806@163.com>
2021-11-19 08:27:58 +0800

    Add files via upload

---
+++ 数字二进制位反转.c
@@ -0,0 +1,25 @@
+﻿#include <stdio.h>
+unsigned  int reverse_bit(unsigned int value)
+{
+	int i = 0;
+	unsigned int n = 0;
+	for(i = 0;i < 31;i++)
+	{
+		n |= value;
+		//int k = value & 1;
+		//n |= k;
+		n <<= 1;
+		//printf("%d",n);
+		value >>= 1;
+	}
+	return n;
+}
+int main()
+{
+	unsigned int value = 0;
+	scanf("%u",&value);
+	//reverse_bit(value);
+	printf("%u\n",reverse_bit(value));
+	//printf("%d\n",sizeof(unsigned int));
+	return 0;
+}
