530d790baa24de18d071f19d0c239ca1afdbcc00
ly132806 <ly132806@163.com>
2021-11-20 20:10:02 +0800

    Add files via upload

---
+++ 建函数实现strcpy.c
@@ -0,0 +1,21 @@
+﻿#include <stdio.h>
+#include <assert.h>
+char * my_strcay(char *arr,const  char *brr)
+{
+	assert(*arr != NULL);
+	assert(*brr != NULL);
+	char * ret = arr;
+	while(*arr++ = *brr++)
+	{
+		;
+	}
+	return ret;
+}
+int main()
+{
+	char arr1[20] = "a,b,c,d,e,f,g,h,i,j,k,l,m,n)";
+	char arr2[] = "hallo";
+	//my_strcay(arr1 ,arr2);
+	printf("%s",my_strcay(arr1 ,arr2));
+	return 0;
+}
