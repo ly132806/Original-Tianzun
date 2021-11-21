a96715ebbb8f1210b61c50873cfa16e7310794bd
ly132806 <ly132806@163.com>
2021-11-21 19:21:36 +0800

    Add files via upload

---
+++ 建函数实现strlen.c
@@ -0,0 +1,19 @@
+﻿#include <stdio.h>
+#include <string.h>
+#include <assert.h>
+int my_strlen(const char *arr)
+{
+	assert(*arr != NULL);
+	int n = 0;
+	while(*arr++ != '\0')
+	{
+		n++;
+	}
+	return n;
+}
+int main()
+{
+	char arr[] = "ffghgfdydcf";
+	printf("%d\n",my_strlen(arr));
+	return 0;
+}
