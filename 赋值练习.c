7b054ebf3eb7965a8c976df082d4c18c3de78083
ly132806 <ly132806@163.com>
2021-11-23 22:14:18 +0800

    Add files via upload

---
+++ 赋值练习.c
@@ -0,0 +1,16 @@
+﻿#include <stdio.h>
+int main()
+{
+	int a = 1,b = 2,c = 2;
+	while(a < b < c)
+	{
+		int t;
+		t = a;
+		a = b;
+		b = t;
+		c--;
+		printf("%d %d %d " ,a,b,c);
+		
+	}
+	return 0;
+}
