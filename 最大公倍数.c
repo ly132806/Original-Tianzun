d905a71e275c64044d30b04d89a0ecd55a1ffc2a
ly132806 <ly132806@163.com>
2021-11-24 22:37:43 +0800

    Add files via upload

---
+++ 最大公倍数.c
@@ -0,0 +1,35 @@
+﻿#include <stdio.h>
+//int main()
+//{
+/*	int m = 0,a,b,i;
+	scanf("%d %d",&a,&b);
+	for(i = 1;;i++)
+	{
+		if(a * i % b == 0)
+		{
+			printf("%d\n",a*i);
+			break;
+		}
+		
+	}
+	*/
+//	#include<stdio.h>
+int main()
+{
+int a,b,c,n,m;
+scanf("%d %d",&a,&b);
+n=a;m=b;
+while(b)
+{
+     printf("%d %d %d ",a ,b,c);
+c=a%b;
+a=b;
+b=c;
+ 
+ printf("%d %d %d\n ",a ,b,c);
+}
+printf("%d\n",n*m/a);
+//return 0;
+//}2
+	return 0;
+}
